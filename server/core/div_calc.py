8edfead899b805595edcd3fc2edf76d409fd7acb
dmt0768 <63654413+dmt0768@users.noreply.github.com>
2020-10-17 12:41:39 +0300

    calc_change

--- server/core/div_calc.py
+++ server/core/div_calc.py
@@ -1,95 +1,147 @@
 from decimal import Decimal
 from fractions import Fraction
 
-def get_multiplier(fr_out: str, fr_in: str = '20000000', delta: float = 0.0001, des_error: float = 0.1, max_error: float = 30, divider_limit : float = 5):
-    flag = True
+def get_multiplier(fr_out: str, fr_in: str = '20000000', delta: float = 0.001, des_error: float = 0.1, max_error: float = 30, divider_limit : float = 5):
 
-    # des_error - желаемая ошибка
-    # delta - шаг при подборе числа, большой -- плохо, маленький -- долго
+    for cheat in [0.1,0.5,1,2,3,4,5,6]:
 
-    fr_in = Decimal(fr_in)  # Специальный класс для точного расчёта
-    fr_out = Decimal(fr_out)
+        flag = True
 
-    res = Fraction(fr_out/fr_in).limit_denominator()  # Ищем наименьшую дробь
+        # des_error - желаемая ошибка
+        # delta - шаг при подборе числа, большой -- плохо, маленький -- долго
 
-    num = res.numerator  # Промежеточные переменные
-    den = res.denominator
+        fr_in = Decimal(fr_in)  # Специальный класс для точного расчёта
+        fr_out = Decimal(fr_out)
 
-    i = 1
+        res = Fraction(fr_out/fr_in).limit_denominator()  # Ищем наименьшую дробь (2 * 10**3 < float(fr_in) / (res.denominator * 2**i / res['N1_HS']) < 2 * 10**6)
 
-    # Быстрый алгоритм, но грубый
-    if (num > 2**20) or (den > 2**39):
-        while (num > 2**20) or (den > 2**39):
-            num = res.numerator / 2**i
-            den = res.denominator / 2**i
-            i += 0.1
-    else:
-        while (num < 2**20 - 1000) and (den < 2**39 - 1000):
-            num = res.numerator * 2**i
-            den = res.denominator * 2**i
-            i += 0.1
 
-    res = {'N1_HS': 4, 'N1_LS': 1, 'N2_HS': 4, 'N2_LS': 1, 'N3': 1}
-    res['N2_LS'] = num / res['N2_HS']
-    res['N3'] = den / res['N1_HS']
 
-    temp = res.copy()
-
-    while not (4.9 * 10**9 < (float(fr_in) * temp['N2_HS'] * temp['N2_LS'] / temp['N3']) <= 5.63 * 10**9):
-        if temp['N1_HS'] == 11:
-            temp['N1_HS'] = 4
-
-            if temp['N1_LS'] == 1:
-                temp['N1_LS'] += 1
+        # Быстрый алгоритм, но грубый
+        def fast_alg(num, den):
+            i = 1
+            if (num > cheat*2**20) or (den > cheat*2**39):
+                while (num > cheat*2**20) or (den > cheat*2**39):
+                    num = res.numerator / 2**i
+                    den = res.denominator / 2**i
+                    i += 0.1
             else:
-                temp['N1_LS'] += 2
+                while (num < cheat*2**20) and (den < cheat*2**39):
 
-        temp['N1_HS'] += 1
-        temp['N3'] = res['N3'] / (temp['N1_HS'] / 4) / temp['N1_LS']  # Тут мы окончательно определяем N1_LS, дальше работаем с с N3 и N2
+                    num = res.numerator * 2**i
+                    den = res.denominator * 2**i
+                    i += 0.1
+            return num, den
 
-        if temp['N3'] < 2:
-            flag = 'PLL out frequency calculation failed!'
-            break
+        temp = fast_alg(res.numerator, res.denominator)
 
-    res = temp.copy()
+        res = {'N1_HS': 4, 'N1_LS': 1, 'N2_HS': 4, 'N2_LS': 1, 'N3': 1}
+        res['N2_LS'] = temp[0] / res['N2_HS']
+        res['N3'] = temp[1] / res['N1_HS']
 
-    # Медленный алгоритм
-    j = 1 + delta
-    k = 1
-    while (abs(float(fr_out) - float(fr_in) * res['N2_HS'] * round(res['N2_LS']) / (res['N1_HS'] * res['N1_LS'] * round(res['N3']))) > k * des_error) or (round(res['N2_LS']) % 2):
-        res['N2_LS'] = temp['N2_LS'] / j
-        res['N3'] = temp['N3'] / j
-        j += delta
-        if j > divider_limit:
-            j = 1
-            k *= 2
-            res['N2_LS'] = temp['N2_LS']
-            res['N3'] = temp['N3']
-            if k * des_error > max_error:
-                flag = 'Fine calculation failed!'
-                break
 
-    res = {'N1_HS': round(res['N1_HS']), 'N1_LS': round(res['N1_LS']), 'N2_HS': round(res['N2_HS']), 'N2_LS': round(res['N2_LS']), 'N3': round(res['N3'])}
+        def PLL_out_alg(dictionary):
+            N1_HS = dictionary['N1_HS']
+            N1_LS = dictionary['N1_LS']
+            N2_HS = dictionary['N2_HS']
+            N2_LS = dictionary['N2_LS']
+            N3 = dictionary['N3']
+            nonlocal flag
+            const_N3 = N3
+            while not (4.9 * 10**9 < (float(fr_in) * N2_HS *  N2_LS / N3) <= 5.63 * 10**9):
+                if N1_HS == 11:
+                    N1_HS = 4
 
-    #  Финальная проверка делителей
-    if flag:
-        if not (4 <= res['N1_HS'] <= 11):
-            flag = 'Divider test №1 failed!'
-        if not (1 <= res['N1_LS'] <= 2**20) and ((res['N1_LS'] % 2 == 0) or res['N1_LS'] == 1):
-            flag = 'Divider test №2 failed!'
-        if not (4 <= res['N2_HS'] <= 11):
-            flag = 'Divider test №3 failed!'
-        if not ((2 <= res['N2_LS'] <= 2**20) and res['N2_LS'] % 2 == 0):
-            flag = 'Divider test №4 failed!'
-        if not (1 <= res['N3'] <= 2**19):
-            flag = 'Divider test №5 failed!'
+                    if N1_LS == 1:
+                        N1_LS += 1
+                    else:
+                        N1_LS += 2
 
-        #  Финальная проверка внутренних частот
-        if not (2 * 10**3 < (float(fr_in) / res['N3']) < 2 * 10**6):
-            flag = 'PLL in frequency test failed!'
+                N1_HS += 1
+                N3 = const_N3 / (N1_HS / 4) / N1_LS  # Тут мы окончательно определяем N1_LS, дальше работаем с с N3 и N2
 
-        if not (4.85 * 10 ** 9 < (float(fr_in) * temp['N2_HS'] * temp['N2_LS'] / temp['N3']) <= 5.67 * 10 ** 9):
-            flag = 'PLL out frequency test failed!'
+                if N3 < 2:
+                    flag = 'PLL out frequency calculation failed!'
+                    break
+            return {'N1_HS': N1_HS, 'N1_LS': N1_LS, 'N2_HS': N2_HS, 'N2_LS': N2_LS, 'N3': N3}
+
+        res = PLL_out_alg(res)
+        temp = res.copy()
+
+        def increase_N3_alg(dictionary):
+            N1_HS = dictionary['N1_HS']
+            N1_LS = dictionary['N1_LS']
+            N2_HS = dictionary['N2_HS']
+            N2_LS = dictionary['N2_LS']
+            N3 = dictionary['N3']
+            const_N3 = N3
+            while (N3 * (N2_HS+1) / 4 < N2_LS) and (N2_HS <= 10) and (2 * 10**3 < (float(fr_in) / N3) < 2 * 10**6):
+                N2_HS += 1
+                N3 = const_N3 * N2_HS / 4
+            return {'N1_HS': N1_HS, 'N1_LS': N1_LS, 'N2_HS': N2_HS, 'N2_LS': N2_LS, 'N3': N3}
+
+        res = increase_N3_alg(res)
+        temp = res.copy()
+
+
+        # Медленный алгоритм
+        def slow_alg(dictionary):
+            N1_HS = dictionary['N1_HS']
+            N1_LS = dictionary['N1_LS']
+            N2_HS = dictionary['N2_HS']
+            N2_LS = dictionary['N2_LS']
+            N3 = dictionary['N3']
+            nonlocal flag
+            const_N2_LS = N2_LS
+            const_N3 = N3
+            j = 1 + delta
+            k = 1
+            while (abs(float(fr_out) - float(fr_in) * N2_HS * round(N2_LS) / (N1_HS * N1_LS * round(N3))) > k * des_error) or (round(N2_LS) % 2):
+
+                N2_LS = const_N2_LS / j
+                N3 = const_N3 / j
+                j += delta
+                if j > divider_limit:
+                    j = 1
+                    k *= 2
+                    N2_LS = const_N2_LS
+                    N3 = const_N3
+                    if k * des_error > max_error:
+                        flag = 'Fine calculation failed!'
+                        break
+
+            return {'N1_HS': N1_HS, 'N1_LS': N1_LS, 'N2_HS': N2_HS, 'N2_LS': N2_LS, 'N3': N3}
+
+
+        if flag == True:
+            res = slow_alg(res)
+
+            res = {'N1_HS': round(res['N1_HS']), 'N1_LS': round(res['N1_LS']), 'N2_HS': round(res['N2_HS']), 'N2_LS': round(res['N2_LS']), 'N3': round(res['N3'])}
+
+        #  Финальная проверка делителей
+
+        if flag == True:
+            if not (4 <= res['N1_HS'] <= 11):
+                flag = 'Divider test №1 failed!'
+            if not (1 <= res['N1_LS'] <= 2**20) and ((res['N1_LS'] % 2 == 0) or res['N1_LS'] == 1):
+                flag = 'Divider test №2 failed!'
+            if not (4 <= res['N2_HS'] <= 11):
+                flag = 'Divider test №3 failed!'
+            if not ((2 <= res['N2_LS'] <= 2**20) and res['N2_LS'] % 2 == 0):
+                flag = 'Divider test №4 failed!'
+            if not (1 <= res['N3'] <= 2**19):
+                flag = 'Divider test №5 failed!'
+
+            #  Финальная проверка внутренних частот
+            if not (2 * 10**3 < (float(fr_in) / res['N3']) < 2 * 10**6):
+                flag = 'PLL in frequency test failed!'
+
+            if not (4.85 * 10 ** 9 < (float(fr_in) * temp['N2_HS'] * temp['N2_LS'] / temp['N3']) <= 5.67 * 10 ** 9):
+                flag = 'PLL out frequency test failed!'
+
+
+        print(cheat, flag)
+        if flag == True: break  # Cheat cycle
 
     freq = float(fr_in) * res['N2_HS'] * res['N2_LS'] / (res['N1_HS'] * res['N1_LS'] * res['N3'])
     err = float(fr_out) - freq
@@ -114,10 +166,9 @@ def get_multiplier(fr_out: str, fr_in: str = '20000000', delta: float = 0.0001, 
         print('Заданная частота =', fr_out)
         print('Фактическая частота = ', freq)
         print('Отклонение', err)
-        print('Для отладки, j =', j)
         print('Статус:', flag)
 
     return [res, {'freq':freq, 'err':err, 'status': flag}]
 
 if __name__ == '__main__':
-    get_multiplier('1'+'000'+'000')
+    get_multiplier('1'+'000'+'006')
--- server/core/div_calc2.py
+++ server/core/div_calc2.py
@@ -1,69 +1,154 @@
 from decimal import Decimal
 from fractions import Fraction
 
-def get_multiplier(fr_out: str, fr_in: str = '20000000', delta: float = 0.0001, des_error: float = 0.1, max_error: float = 20, divider_limit : float = 10):
+def get_multiplier(fr_out: str, fr_in: str = '20000000', delta: float = 0.0001, des_error: float = 0.1, max_error: float = 30, divider_limit : float = 5, cheat : int = 1):
     flag = True
-    res = {'N1_HS':4, 'N1_LS':1, 'N2_HS':4, 'N2_LS':1, 'N3':1}
+
     # des_error - желаемая ошибка
     # delta - шаг при подборе числа, большой -- плохо, маленький -- долго
 
     fr_in = Decimal(fr_in)  # Специальный класс для точного расчёта
     fr_out = Decimal(fr_out)
 
-    res = Fraction(fr_out/fr_in).limit_denominator()  # Ищем наименьшую дробь
-    #print('Результат 1:', res)
+    res = Fraction(fr_out/fr_in).limit_denominator()  # Ищем наименьшую дробь (2 * 10**3 < float(fr_in) / (res.denominator * 2**i / res['N1_HS']) < 2 * 10**6)
 
-    num = res.numerator  # Промежеточные переменные
-    den = res.denominator
 
-    i = 1
-    j = 1 + delta  # Счётчики
 
     # Быстрый алгоритм, но грубый
+    def fast_alg(num, den):
+        i = 1
+        if (num > cheat*2**20) or (den > cheat*2**39):
+            while (num > cheat*2**20) or (den > cheat*2**39):
+                num = res.numerator / 2**i
+                den = res.denominator / 2**i
+                i += 0.1
+        else:
+            while (num < cheat*2**20) and (den < cheat*2**39):
 
-    while (num > 2**20) or (den > 2**39):
-        num = res.numerator / i
-        den = res.denominator / i
-        i += 1
+                num = res.numerator * 2**i
+                den = res.denominator * 2**i
+                i += 0.1
+        return num, den
 
-    if den > 2**20:
-        print('Algorithm needs to be more advanced')
+    temp = fast_alg(res.numerator, res.denominator)
 
-    res = {'N1_HS': 1, 'N1_LS': 1, 'N2_HS': 1, 'N2_LS': num, 'N3': den}
+    res = {'N1_HS': 4, 'N1_LS': 1, 'N2_HS': 4, 'N2_LS': 1, 'N3': 1}
+    res['N2_LS'] = temp[0] / res['N2_HS']
+    res['N3'] = temp[1] / res['N1_HS']
 
-    num1 = num  # Ещё одни промежуточные переменные
-    den1 = den
 
-    # Медленный алгоритм
-    temp = 1
-    while abs(fr_out - fr_in * round(num)/(round(den))) > temp * des_error:
-        num = num1 / j
-        den = den1 / j
-        j += delta
-        if j > divider_limit:
-            j = 1
-            temp += 1
-            num = num1
-            den = den1
-            if temp * des_error > max_error:
-                print('Calculation failed!')
-                flag = False
+    def PLL_out_alg(dictionary):
+        N1_HS = dictionary['N1_HS']
+        N1_LS = dictionary['N1_LS']
+        N2_HS = dictionary['N2_HS']
+        N2_LS = dictionary['N2_LS']
+        N3 = dictionary['N3']
+        nonlocal flag
+        const_N3 = N3
+        while not (4.9 * 10**9 < (float(fr_in) * N2_HS *  N2_LS / N3) <= 5.63 * 10**9):
+            if N1_HS == 11:
+                N1_HS = 4
+
+                if N1_LS == 1:
+                    N1_LS += 1
+                else:
+                    N1_LS += 2
+
+            N1_HS += 1
+            N3 = const_N3 / (N1_HS / 4) / N1_LS  # Тут мы окончательно определяем N1_LS, дальше работаем с с N3 и N2
+
+            if N3 < 2:
+                flag = 'PLL out frequency calculation failed!'
                 break
+        return {'N1_HS': N1_HS, 'N1_LS': N1_LS, 'N2_HS': N2_HS, 'N2_LS': N2_LS, 'N3': N3}
 
-    res = {'N1_HS': 1, 'N1_LS': 1, 'N2_HS': 1, 'N2_LS': num, 'N3': den}
-
+    res = PLL_out_alg(res)
     temp = res.copy()
 
+    def increase_N3_alg(dictionary):
+        N1_HS = dictionary['N1_HS']
+        N1_LS = dictionary['N1_LS']
+        N2_HS = dictionary['N2_HS']
+        N2_LS = dictionary['N2_LS']
+        N3 = dictionary['N3']
+        const_N3 = N3
+        while (N3 * (N2_HS+1) / 4 < N2_LS) and (N2_HS <= 10) and (2 * 10**3 < (float(fr_in) / N3) < 2 * 10**6):
+            N2_HS += 1
+            N3 = const_N3 * N2_HS / 4
+        return {'N1_HS': N1_HS, 'N1_LS': N1_LS, 'N2_HS': N2_HS, 'N2_LS': N2_LS, 'N3': N3}
 
-    while not (4.85 * 10**9 < (float(fr_in) * temp['N2_HS'] * temp['N2_LS'] / temp['N3']) <= 5.67 * 10**9):
+    res = increase_N3_alg(res)
+    temp = res.copy()
+    freq = float(fr_in) * res['N2_HS'] * res['N2_LS'] / (res['N1_HS'] * res['N1_LS'] * res['N3'])
+    print(float(fr_out) - freq)
 
-        temp['N1_LS'] += 1
-        temp['N3'] = res['N3'] / temp['N1_LS']
+    # Медленный алгоритм
+    def slow_alg(dictionary):
+        N1_HS = dictionary['N1_HS']
+        N1_LS = dictionary['N1_LS']
+        N2_HS = dictionary['N2_HS']
+        N2_LS = dictionary['N2_LS']
+        N3 = dictionary['N3']
+        nonlocal flag
+        const_N2_LS = N2_LS
+        const_N3 = N3
+        j = 1 + delta
+        k = 1
+        while (abs(float(fr_out) - float(fr_in) * N2_HS * round(N2_LS) / (N1_HS * N1_LS * round(N3))) > k * des_error) or (round(N2_LS) % 2):
 
-    res = {'N1_HS': 4, 'N1_LS': temp['N1_LS'], 'N2_HS': 4, 'N2_LS': round(num), 'N3': round(temp['N3'])}
+            N2_LS = const_N2_LS / j
+            N3 = const_N3 / j
+            j += delta
+            if j > divider_limit:
+                j = 1
+                k *= 2
+                N2_LS = const_N2_LS
+                N3 = const_N3
+                if k * des_error > max_error:
+                    flag = 'Fine calculation failed!'
+                    break
+
+        return {'N1_HS': N1_HS, 'N1_LS': N1_LS, 'N2_HS': N2_HS, 'N2_LS': N2_LS, 'N3': N3}
+
+
+    if flag == True:
+        res = slow_alg(res)
+
+        res = {'N1_HS': round(res['N1_HS']), 'N1_LS': round(res['N1_LS']), 'N2_HS': round(res['N2_HS']), 'N2_LS': round(res['N2_LS']), 'N3': round(res['N3'])}
+
+    #  Финальная проверка делителей
+
+    if flag == True:
+        if not (4 <= res['N1_HS'] <= 11):
+            flag = 'Divider test №1 failed!'
+        if not (1 <= res['N1_LS'] <= 2**20) and ((res['N1_LS'] % 2 == 0) or res['N1_LS'] == 1):
+            flag = 'Divider test №2 failed!'
+        if not (4 <= res['N2_HS'] <= 11):
+            flag = 'Divider test №3 failed!'
+        if not ((2 <= res['N2_LS'] <= 2**20) and res['N2_LS'] % 2 == 0):
+            flag = 'Divider test №4 failed!'
+        if not (1 <= res['N3'] <= 2**19):
+            flag = 'Divider test №5 failed!'
+
+        #  Финальная проверка внутренних частот
+        if not (2 * 10**3 < (float(fr_in) / res['N3']) < 2 * 10**6):
+            flag = 'PLL in frequency test failed!'
+
+        if not (4.85 * 10 ** 9 < (float(fr_in) * temp['N2_HS'] * temp['N2_LS'] / temp['N3']) <= 5.67 * 10 ** 9):
+            flag = 'PLL out frequency test failed!'
 
     freq = float(fr_in) * res['N2_HS'] * res['N2_LS'] / (res['N1_HS'] * res['N1_LS'] * res['N3'])
     err = float(fr_out) - freq
+
+    #  Подготовка к отправке
+    #  Учтём, например, что 0 в регистре N1_HS означает -- 4 в делителе
+
+    res['N1_HS'] = res['N1_HS'] - 4
+    res['N1_LS'] = res['N1_LS'] - 1
+    res['N2_HS'] = res['N2_HS'] - 4
+    res['N2_LS'] = res['N2_LS'] - 1
+    res['N3'] = res['N3'] - 1
+
     # Проверим наш код
     if __name__ == '__main__':
         print('N1_HS =', res['N1_HS'])
@@ -71,23 +156,13 @@ def get_multiplier(fr_out: str, fr_in: str = '20000000', delta: float = 0.0001, 
         print('N2_HS =', res['N2_HS'])
         print('N2_LS =', res['N2_LS'])
         print('N3 =', res['N3'])
-        freq = float(fr_in) * res['N2_HS'] * res['N2_LS'] / (res['N1_HS'] * res['N1_LS'] * res['N3'])
+
         print('Заданная частота =', fr_out)
         print('Фактическая частота = ', freq)
         print('Отклонение', err)
-        print('Для отладки, j =', j)
-
-
+        print('Статус:', flag)
 
     return [res, {'freq':freq, 'err':err, 'status': flag}]
 
-
 if __name__ == '__main__':
-    get_multiplier('600'+'342'+'200')
-    '''
-    for i in range(3*10**6,10**7):
-        a = get_multiplier(str(i))
-        print(i, a[1]['err'])
-        if abs(a[1]['err']) >= 20:
-            break
-    '''
+    get_multiplier('555'+'555'+'000')
