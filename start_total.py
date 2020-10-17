f5b4d8c400b16a4c6c231e88ede145bc19d0b11a
Demo User <debian@beaglebone.localdomain>
2020-10-17 10:58:50 +0000

    start_total

--- start_total.py
+++ start_total.py
@@ -1,6 +1,9 @@
 #!/home/debian/server_synth/venv/bin/python3
 import os
+import time
 
+time.sleep(20)
+os.system('git -C /home/debian/server_synth/ pull')
 os.system('/home/debian/server_synth/synth_init.py')
 os.system('/home/debian/server_synth/server/manage.py runserver 192.168.7.2:8080')
 
