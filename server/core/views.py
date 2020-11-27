7e84592b54f2c123a99f30506579277b4475fe6c
dmt0768 <63654413+dmt0768@users.noreply.github.com>
2020-11-27 16:35:53 +0300

    other_dividers_start

--- server/core/views.py
+++ server/core/views.py
@@ -30,7 +30,7 @@ set_inc_write = int('01100000',2)  # Запись с инкрементом ад
 first_adr = 0  # Самый первый номер регистра
 
 
-#_______________________________________
+#_______________________________________ Functions-helpers
 def is_integer(n):
     try:
         int(n)
@@ -44,16 +44,12 @@ def format_int(x):
 
 
 def invert_int(x):
-    '''
-    ans = ''
-    print(format(x, '08b'))
-    for i in bin(x)[2:]:
-        if i == '1':
-            ans += '0'
-        else:
-            ans += '1'
-    '''
     return ~x&255
+
+
+def make_ICAL():
+    spi.writebytes([set_adr, 136])  # Установка очередного адреса
+    spi.writebytes([set_write, 64])  # ICAL
 #_______________________________________
 
 
@@ -82,7 +78,6 @@ def install_default(request):  # Сброс настроек в исходные
         i.status = i.default_status
         i.save()
 
-
     spi.writebytes([set_adr, 136])  # Установка очередного адреса
     spi.writebytes([set_write, 128]) # Reset
     status = ['Reset']
@@ -109,14 +104,47 @@ def show_main_page(request):  # Используется при загрузке
     status = ['OK']
     return render(request, 'core/Main_page.html', {'Lines':content})
 
+
 def refresh_page(request):  # Обновляет меняющуюся часть страницы. Для ПОЛНОЙ перезагрузи страницы исп. show_main_page
     global status
     content = Lines.objects.all()
     status = ['OK']
     return render(request, 'core/Create_tmpl.html', {'Lines':content}) #redirect('show_main_page')
 
+######################################## Functions-helpers
+
+
+def helper_edit_line_write_regs(reg):
+    for i in reg:
+        spi.writebytes([set_adr, i])  # Установка очередного адреса
+        spi.writebytes([set_write, reg[i]])
+
+
+def helper_edit_line_write_bd(reg):
+    for i in reg:
+        j = Registers.objects.filter(id=i)[0]
+        j.value = format_int(reg[i])
+        j.save()
+
+
+def helper_edit_line_get_N1_LS():
+    step = 1  # 0 is 1, 1 is 2, 2 is 3. but 0 + step is 1
+    reg = {}
+    reg[25] = 0
+    reg[26] = 0
+    reg[27] = 0
+    for i in reg:
+        reg[i] = int(Registers.objects.filter(id=i)[0].value[:-1], 16)
+
+    reg[26] = reg[26] << 8
+    reg[25] = (reg[25] & 0xF) << 16
+
+    return sum(reg.values()) + step
+##########################################
+
 
 def edit_line(request):  # Отправка данный в синтезатор
+
     global status
     #print('\n\n\n\n\n' + str(request.GET) + '\n\n\n\n\n')
     edit = Lines.objects.filter(id=int(request.GET['edit']))[0]
@@ -131,51 +159,51 @@ def edit_line(request):  # Отправка данный в синтезатор
         edit.output_freq = int(temp['output_freq'])
         edit.status = 'Активно'
         edit.save()
-    res = get_multiplier(temp['output_freq'], temp['input_freq'])
-    reg = {}
-    if res[1]['status']:
-        reg[25] = 0
-        reg[26] = 0
-        reg[27] = 0
-        reg[25] |= res[0]['N1_HS'] << 5
-        reg[25] |= int(format(res[0]['N1_LS'], '020b')[0:4], 2)
-        reg[26] |= int(format(res[0]['N1_LS'], '020b')[4:12], 2)
-        reg[27] |= int(format(res[0]['N1_LS'], '020b')[12:], 2)
 
-        reg[40] = 0
-        reg[41] = 0
-        reg[42] = 0
-        reg[40] |= res[0]['N2_HS'] << 5
-        reg[40] |= int(format(res[0]['N2_LS'], '020b')[0:4], 2)
-        reg[41] |= int(format(res[0]['N2_LS'], '020b')[4:12], 2)
-        reg[42] |= int(format(res[0]['N2_LS'], '020b')[12:], 2)
+    if int(request.GET['edit']) == 1:  # Main channel
+        res = get_multiplier(temp['output_freq'], temp['input_freq'])
+        reg = {}
+        if res[1]['status']:
+            reg[25] = 0
+            reg[26] = 0
+            reg[27] = 0
+            reg[25] |= res[0]['N1_HS'] << 5
+            reg[25] |= int(format(res[0]['N1_LS'], '020b')[0:4], 2)
+            reg[26] |= int(format(res[0]['N1_LS'], '020b')[4:12], 2)
+            reg[27] |= int(format(res[0]['N1_LS'], '020b')[12:], 2)
 
-        reg[43] = 0
-        reg[44] = 0
-        reg[45] = 0
-        reg[43] |= int(format(res[0]['N3'], '019b')[0:3], 2)
-        reg[44] |= int(format(res[0]['N3'], '019b')[3:11], 2)
-        reg[45] |= int(format(res[0]['N3'], '019b')[11:], 2)
-        for i in reg:
-            spi.writebytes([set_adr, i])  # Установка очередного адреса
-            spi.writebytes([set_write, reg[i]])
-        for i in reg:
-            j = Registers.objects.filter(id=i)[0]
-            print(j.value)
-            j.value = format_int(reg[i])
-            j.save()
-            print(j.value)
+            reg[40] = 0
+            reg[41] = 0
+            reg[42] = 0
+            reg[40] |= res[0]['N2_HS'] << 5
+            reg[40] |= int(format(res[0]['N2_LS'], '020b')[0:4], 2)
+            reg[41] |= int(format(res[0]['N2_LS'], '020b')[4:12], 2)
+            reg[42] |= int(format(res[0]['N2_LS'], '020b')[12:], 2)
 
-        spi.writebytes([set_adr, 136])  # Установка очередного адреса
-        spi.writebytes([set_write, 64])  # ICAL
-        status = res
+            reg[43] = 0
+            reg[44] = 0
+            reg[45] = 0
+            reg[43] |= int(format(res[0]['N3'], '019b')[0:3], 2)
+            reg[44] |= int(format(res[0]['N3'], '019b')[3:11], 2)
+            reg[45] |= int(format(res[0]['N3'], '019b')[11:], 2)
 
+            helper_edit_line_write_regs(reg)
+            make_ICAL()
+            helper_edit_line_write_bd(reg)
+
+            status = res
+
+        else:
+            status = 'Failed'
 
     else:
-        status = 'Failed'
+        main_freq = Lines.objects.filter(id=1)[0].output_freq
+        helper_edit_line_get_N1_LS()
+        print(helper_edit_line_get_N1_LS())
+        print(main_freq)
 
     content = Lines.objects.all()
-    return render(request, 'core/Create_tmpl.html', {'Lines':content}) #redirect('show_main_page')
+    return render(request, 'core/Create_tmpl.html', {'Lines':content})
 
 
 def stop_line(request):  # Подача 0 на выход синтезатора
@@ -199,7 +227,7 @@ def turn_on(request):  # Регистр включения-выключения 
         content.turn_on = 1
     else:
         content.turn_on = 0
-    print(content.turn_on)
+    #print(content.turn_on)
     content.save()
     if content.turn_on == 0:
         one = 1 << (int(request.GET['edit'])-1)
@@ -219,7 +247,7 @@ def turn_on(request):  # Регистр включения-выключения 
         reg = Registers.objects.filter(id=10)[0]
         reg.value = format_int(temp)
         reg.save()
-    print(temp)
+    #print(temp)
     spi.writebytes([set_adr, 10])  # адрес DSBL5_REG
     spi.writebytes([set_write, temp])
     return HttpResponse()
